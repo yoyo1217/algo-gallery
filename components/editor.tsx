c2dee94d3c9fb9a7cf74ba70b75efead03ef5776
yoyo1217 <[id+username]@users.noreply.github.com>
2023-12-04 21:21:42 +1100

    added editor on knapsack page

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,6 +1,20 @@
 "use client";
 
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { okaidia } from "@uiw/codemirror-theme-okaidia";
+import knapsack from "@/code-text/knapsack.txt";
+
+const extensions = [javascript({ jsx: true })];
+
 const Editor = () => {
-  return <div>Editor</div>;
+  return (
+    <CodeMirror
+      value={knapsack}
+      theme={okaidia}
+      extensions={[javascript({ jsx: true })]}
+      readOnly={true}
+    />
+  );
 };
 export default Editor;
