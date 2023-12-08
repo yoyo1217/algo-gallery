2726f49b359b851644b80df90a014ab8bc2c0a39
yoyo1217 <[id+username]@users.noreply.github.com>
2023-12-08 21:24:47 +1100

    add github link and remove menu link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,37 +1,33 @@
 "use client";
 import useOutsideClick from "@/hooks/useOutsideClick";
+import Image from "next/image";
 import Link from "next/link";
 import { useRef, useState } from "react";
+import Hamburger from "./Hamburger";
+import { AnimatePresence, motion } from "framer-motion";
+import { routes } from "@/utils/routes";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-
-  useOutsideClick(menuRef, () => {
-    if (isOpen) setIsOpen(false);
-  });
+  // const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="w-full bg-gray-800 text-white p-4 relative" ref={menuRef}>
-      <div className="container mx-auto flex items-center justify-between ">
-        <Link href="/" className="text-xl font-bold">
-          Algorithm Visualization
-        </Link>
-        <button onClick={() => setIsOpen(!isOpen)}>Menu</button>
-      </div>
-      {isOpen && (
-        <div className="flex flex-col absolute top-16 right-0 bg-gray-700 p-4 rounded-lg shadow-md2">
-          <Link href="/" className="mt-2">
-            Home
-          </Link>
-          <Link href="/monte-carlo-simulator" className="mt-2">
-            Monte Carlo Simulator
-          </Link>
-          <Link href="/knapsack" className="mt-2">
-            Knapsack Simulator
+    <nav className="w-full bg-gray-800 text-white py-4 relative">
+      <div className=" flex items-center justify-between text-2xl md:text-5xl font-bold">
+        <div className="pl-2">
+          <Link href="/">Algo Gallery</Link>
+        </div>
+        <div className="flex items-center pr-3 gap-4">
+          <Link href="https://github.com/yoyo1217/algo-gallery">
+            <Image
+              className="w-8 h-8 md:w-12 md:h-12"
+              width={48}
+              height={48}
+              src="/github-mark-white.svg"
+              alt="github Invertocat Logo"
+            />
           </Link>
         </div>
-      )}
+      </div>
     </nav>
   );
 };
---
+++ utils/routes.ts
@@ -0,0 +1,12 @@
+export const routes = [
+  {
+    title: "Monte Carlo Simulator",
+    link: "/monte-carlo-simulator",
+    imageUrl: "/monte.png",
+  },
+  {
+    title: "Knapsack",
+    link: "/knapsack",
+    imageUrl: "/knapsack.png",
+  },
+];
