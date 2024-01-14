5dc0eafcaae6d373e10f5d63bd2133f6cb30a071
yoyo1217 <[id+username]@users.noreply.github.com>
2024-01-14 20:22:01 +1100

    changed modal implementation

--- app/(main)/game-of-life/_components/Modal.tsx
+++ app/(main)/game-of-life/_components/Modal.tsx
@@ -1,15 +1,31 @@
 import Link from "next/link";
 import Button from "./Button";
 import { gliderGun, glinder } from "../pattern";
+import { useEffect, useRef } from "react";
 
 type ModalProps = {
+  onClose: () => void;
   drawLifeGrid: (life: number[][]) => void;
 };
 
-const Modal: React.FC<ModalProps> = ({ drawLifeGrid }) => {
+const Modal: React.FC<ModalProps> = ({ onClose, drawLifeGrid }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        {
+          onClose();
+        }
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [onClose]);
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
-      <div className="p-8 border w-96 shadow-lg rounded-md bg-white">
+      <div className="p-8 border w-96 shadow-lg rounded-md bg-white" ref={ref}>
         <div className="text-center">
           <h3 className="text-2xl font-bold text-gray-900">Life Lexicon</h3>
           <div className="flex flex-col items-center justify-content gap-y-2 mt-2 px-7 py-3">
@@ -20,12 +36,7 @@ const Modal: React.FC<ModalProps> = ({ drawLifeGrid }) => {
             />
           </div>
           <div className="flex justify-center mt-4">
-            <Link
-              href="/game-of-life"
-              className="px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-300"
-            >
-              Close
-            </Link>
+            <Button label="Close" onClick={onClose} />
           </div>
         </div>
       </div>
